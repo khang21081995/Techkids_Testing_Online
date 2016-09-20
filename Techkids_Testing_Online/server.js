1e8d46b485a2e43e622690397077b3caf40b443e
khang21081995 <khang21081995@gmail.com>
2016-09-20 18:24:14 +0700

    e

--- Techkids_Testing_Online/server.js
+++ Techkids_Testing_Online/server.js
@@ -2,10 +2,14 @@
 
 var http = require("http");
 var express = require("express");
-var bodyParser = require('body-parser')
-
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var parseurl = require('parseurl');
+var cookieParser = require('cookie-parser')
+var mySecret = "techkids";
 /******************************** Mongo DB ***************************/
 var mongoose = require('mongoose');
+//mongoose.connect('mongodb://admin:admin@ds033126.mlab.com:33126/techkids-testing-online');
 mongoose.connect('mongodb://admin:admin@ds033126.mlab.com:33126/techkids-testing-online');
 
 var db = mongoose.connection;
@@ -16,10 +20,37 @@ db.once('open', function () {
 
 /******************************** Express App ************************/
 var app = express();
+
 app.use(express.static(__dirname + "/client"));
 app.use(bodyParser.json());
-
+app.use(cookieParser('techkids', {maxAge: 120}));
+app.use(session({
+  secret: mySecret
+}));
 require('./routes')(app);
 
+/******************************** Session ****************************/
+ app.use(function (req, res, next) {
+   req.session.views;
+
+   if (!req.session.views) {
+     req.session.views = {}
+   }
+
+   // get the url pathname
+   var pathname = parseurl(req).pathname;
+
+   // count the views
+   req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+
+
+   next()
+ });
+ 
+ app.get('/foo', function (req, res, next) {
+   console.log(req.cookies);
+   res.cookie("remember", true);
+   res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+ });
 
 http.createServer(app).listen(6789);
